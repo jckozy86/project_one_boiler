9877e626b7be676ce4e2b3f64c25d62542addfbc
Yangyin Hu <yangyinhu@Toms-MacBook-Pro.local>
2019-04-07 02:02:27 -0400

    user input

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,29 +1,29 @@
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCbaDBRoB73Wo-g9jMMjRNXXB7l8PuQjZQ",
-  authDomain: "project-one-f75c6.firebaseapp.com",
-  databaseURL: "https://project-one-f75c6.firebaseio.com",
-  projectId: "project-one-f75c6",
-  storageBucket: "project-one-f75c6.appspot.com",
-  messagingSenderId: "272761884868"
+const config = {
+  apiKey: 'AIzaSyCbaDBRoB73Wo-g9jMMjRNXXB7l8PuQjZQ',
+  authDomain: 'project-one-f75c6.firebaseapp.com',
+  databaseURL: 'https://project-one-f75c6.firebaseio.com',
+  projectId: 'project-one-f75c6',
+  storageBucket: 'project-one-f75c6.appspot.com',
+  messagingSenderId: '272761884868',
 };
 firebase.initializeApp(config);
 
-var database = firebase.database();
+const database = firebase.database();
 
 // 2. Button for adding users
-$("#add-user-btn").on("click", function(event) {
+$('#add-user-btn').on('click', (event) => {
   event.preventDefault();
 
   // Grabs user input
-  var userName = $("#name-input").val().trim();
-  var userBirth = $("#birth-input").val().trim();
+  const userName = $('#name-input').val().trim();
+  const userBirth = $('#birth-input').val().trim();
 
   // user Info
   // console.log('userStart: ' + userStart);
 
   // Creates local "newuser" object for holding user data
-  var newuser = {
+  const newuser = {
     name: userName,
     birth: userBirth,
     dateAdded: firebase.database.ServerValue.TIMESTAMP,
@@ -39,6 +39,6 @@ $("#add-user-btn").on("click", function(event) {
   // alert("user successfully added");
 
   // Clears all of the text-boxes
-  $("#name-input").val("");
-  $("#birth-input").val("");
+  $('#name-input').val('');
+  $('#birth-input').val('');
 });
